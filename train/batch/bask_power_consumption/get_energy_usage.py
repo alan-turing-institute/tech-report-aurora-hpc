d728b4236ecc00e5894a6056f0f50a8235fa84f9
Rosie Wood <rwood@turing.ac.uk>
2026-01-29 17:08:32 +0000

    store outputs to csv and fix for total no. of GPUs

--- train/batch/bask_power_consumption/get_energy_usage.py
+++ train/batch/bask_power_consumption/get_energy_usage.py
@@ -19,25 +19,49 @@ all_out_files.sort(
 configs = set(f.parent.name for f in all_out_files)
 print(f"Configurations found: {configs}")
 
+energy_data = []
+
 for out_file in all_out_files:
     cfg = out_file.parent.name
+    n_gpus = int(cfg.split("x")[0]) * int(cfg.split("x")[1])
 
     with open(out_file) as f:
         text = f.read()
 
     # e.g. Elapsed: 1194 seconds
     elapsed_match = re.search(r"Elapsed\:\s+" + r"([0-9]+(?:\.[0-9]+)?) seconds", text)
+    nnodes = re.search(r"Nodes:\s*(\d+)", text)
+    ngpus = re.search(r"GPUs per node:\s*(\d+)", text)
 
     if elapsed_match:
         elapsed_time = float(elapsed_match.group(1))
     else:
         raise ValueError(f"Elapsed time not found in {out_file}")
 
+    if nnodes and ngpus:
+        nnodes_value = int(nnodes.group(1))
+        ngpus_value = int(ngpus.group(1))
+        total_gpus = nnodes_value * ngpus_value
+    else:
+        raise ValueError(f"Node/GPU info not found in {out_file}")
+
     stats_df = pd.read_csv(f"./{cfg}_avg_gpu_stats.csv", index_col=0)
 
     mean_power_watt = stats_df.loc["mean", "Power (W)"]
 
-    energy_joules = mean_power_watt * elapsed_time
+    energy_joules = mean_power_watt * elapsed_time * total_gpus
     print(
         f"Energy consumption for {cfg}:\n{energy_joules:.2f} Joules over {elapsed_time:.2f} seconds using {mean_power_watt:.2f} Watts average power."
     )
+
+    energy_data.append([cfg, energy_joules, elapsed_time, mean_power_watt])
+
+print(energy_data)
+energy_df = pd.DataFrame(
+    energy_data,
+    columns=["Config", "Energy (J)", "Elapsed Time (S)", "Mean Power (W)"],
+)
+
+energy_df = energy_df.round(2)
+energy_df.to_csv("bask_energy_consumption_summary.csv", index=False)
+print("Energy consumption summary saved to bask_energy_consumption_summary.csv")
